a4d5171cb1bc6f1ea986b50cbe51d306367da57f
DavidLacour <davidlacourgaletto@gmail.com>
2024-10-28 19:34:49 +0100

    forgot to use validation set in early stopping

--- FasterRCNN-PyTorch-main/tools/infer.py
+++ FasterRCNN-PyTorch-main/tools/infer.py
@@ -149,7 +149,7 @@ def compute_map(det_boxes, gt_boxes, iou_threshold=0.5, method='area'):
     return mean_ap, all_aps
 
 
-def load_model_and_dataset(args):
+def load_model_and_dataset(args,validation_set = False):
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     # Read the config file #
     with open(args.config_path, 'r') as file:
@@ -173,8 +173,11 @@ def load_model_and_dataset(args):
         device = torch.device('cpu')
     if device == 'cuda':
         torch.cuda.manual_seed_all(seed)
+    if not val:
+        voc = VOCDataset('test', im_dir=dataset_config['im_test_path'], ann_dir=dataset_config['ann_test_path'],  depth_dir= depth_dir )
+    else: 
+        voc = VOCDataset('test', im_dir=dataset_config['im_val_path'], ann_dir=dataset_config['ann_val_path'],  depth_dir= depth_dir )
     
-    voc = VOCDataset('test', im_dir=dataset_config['im_test_path'], ann_dir=dataset_config['ann_test_path'],  depth_dir= depth_dir )
     test_dataset = DataLoader(voc, batch_size=1, shuffle=False)
     
     faster_rcnn_model = FasterRCNN(model_config, num_classes=dataset_config['num_classes'])
@@ -274,7 +277,7 @@ def infer(args):
         cv2.imwrite('samples/output_frcnn_{}.jpg'.format(sample_count), im)
 
 
-def evaluate_map(args):
+def evaluate_map(args,validation_set = False):
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     if args.forcecpu:
         device = torch.device('cpu')
--- FasterRCNN-PyTorch-main/tools/train.py
+++ FasterRCNN-PyTorch-main/tools/train.py
@@ -197,7 +197,7 @@ def train(args):
             writer.add_scalar('Training/Epoch_Time', epoch_time, epoch)
             
             # Evaluate mAP and handle early stopping
-            map_score = evaluate_map(args)
+            map_score = evaluate_map(args,val=True)
             writer.add_scalar('map', map_score, epoch)
             early_stopping(map_score, epoch)
             
