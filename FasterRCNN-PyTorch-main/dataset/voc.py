305af546d56a14ac0f48364cb70ccf5ef13e5a0a
DavidLacour <davidlacourgaletto@gmail.com>
2024-11-08 11:54:46 +0100

    debug

--- FasterRCNN-PyTorch-main/dataset/voc.py
+++ FasterRCNN-PyTorch-main/dataset/voc.py
@@ -25,8 +25,9 @@ def load_images_and_anns(im_dir, ann_dir,depth_dir, label2idx):
     for ann_file in tqdm(glob.glob(os.path.join(ann_dir, '*.xml'))):
         im_info = {}
         im_info['img_id'] = os.path.basename(ann_file).split('.xml')[0]
-        im_info['filename'] = os.path.join(im_dir, '{}.jpg'.format(im_info['img_id']))
-
+        #im_info['filename'] = os.path.join(im_dir, '{}.jpg'.format(im_info['img_id']))
+        # CHANGE THIS DEBUG 
+        im_info['filename'] = im_info['filename'].replace('distorted', 'undistorted')
         camera_name = '_'.join(im_info['img_id'].split('_')[:3])
         depth_filename = f"{camera_name}_depth.npy"
         depth_path = os.path.join(depth_dir, depth_filename)
