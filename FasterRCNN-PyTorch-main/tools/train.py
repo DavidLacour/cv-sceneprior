5104c0f096664108a42c723a6b2b63d0ced609d0
DavidLacour <davidlacourgaletto@gmail.com>
2024-11-24 14:30:36 +0100

    fix

--- FasterRCNN-PyTorch-main/tools/train.py
+++ FasterRCNN-PyTorch-main/tools/train.py
@@ -285,22 +285,9 @@ def train(args):
             writer.add_scalar('Loss/Epoch/FRCNN_Localization', epoch_frcnn_loc_loss, epoch)
             writer.add_scalar('Training/Epoch_Time', epoch_time, epoch)
 
-            val_loss = 0.0
-            val_loss /= len(val_dataset)
-            val_epoch_rpn_cls_loss = np.mean(rpn_classification_losses)
-            val_epoch_rpn_loc_loss = np.mean(rpn_localization_losses)
-            val_epoch_frcnn_cls_loss = np.mean(frcnn_classification_losses)
-            val_epoch_frcnn_loc_loss = np.mean(frcnn_localization_losses)
+            
 
-            # Log validation loss to TensorBoard
-            writer.add_scalar('Loss/Epoch/RPN_Classification_validation', val_epoch_rpn_cls_loss, epoch)
-            writer.add_scalar('Loss/Epoch/RPN_Localization_validation', val_epoch_rpn_loc_loss, epoch)
-            writer.add_scalar('Loss/Epoch/FRCNN_Classification_validation',  val_epoch_frcnn_cls_loss, epoch)
-            writer.add_scalar('Loss/Epoch/FRCNN_Localization_validation',  val_epoch_frcnn_loc_loss, epoch)
-            writer.add_scalar('Loss/Epoch/total_localization_validation',  val_epoch_rpn_loc_loss +  val_epoch_frcnn_loc_loss, epoch )
-
-            print(f'train loss  {epoch_frcnn_loc_loss}')
-            print(f'val loss  {val_epoch_frcnn_loc_loss}')
+         
 
             with torch.no_grad():  # Disable gradient computation
                 for val_im, val_target, val_fname in tqdm(val_dataloader):
@@ -314,7 +301,23 @@ def train(args):
                     val_rpn_localization_losses.append(val_rpn_output['rpn_localization_loss'])
                     val_frcnn_classification_losses.append(val_frcnn_output['frcnn_classification_loss'])
                     val_frcnn_localization_losses.append(val_frcnn_output['frcnn_localization_loss'])
-                    
+
+            val_loss = 0.0
+            val_loss /= len(val_dataset)
+            val_epoch_rpn_cls_loss = np.mean(val_rpn_classification_losses)
+            val_epoch_rpn_loc_loss = np.mean(val_rpn_localization_losses)
+            val_epoch_frcnn_cls_loss = np.mean(val_frcnn_classification_losses)
+            val_epoch_frcnn_loc_loss = np.mean(val_frcnn_localization_losses)
+
+            # Log validation loss to TensorBoard
+            writer.add_scalar('Loss/Epoch/RPN_Classification_validation', val_epoch_rpn_cls_loss, epoch)
+            writer.add_scalar('Loss/Epoch/RPN_Localization_validation', val_epoch_rpn_loc_loss, epoch)
+            writer.add_scalar('Loss/Epoch/FRCNN_Classification_validation',  val_epoch_frcnn_cls_loss, epoch)
+            writer.add_scalar('Loss/Epoch/FRCNN_Localization_validation',  val_epoch_frcnn_loc_loss, epoch)
+            writer.add_scalar('Loss/Epoch/total_localization_validation',  val_epoch_rpn_loc_loss +  val_epoch_frcnn_loc_loss, epoch )
+                
+            print(f'train loss  {epoch_frcnn_loc_loss}')
+            print(f'val loss  {val_epoch_frcnn_loc_loss}')
             
             # Evaluate mAP and handle early stopping
             #save because evaluate map uses the weights 
