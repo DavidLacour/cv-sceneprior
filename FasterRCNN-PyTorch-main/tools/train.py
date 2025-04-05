154e425d7a9fdac71bb54d07aae518210a8550be
DavidLacour <davidlacourgaletto@gmail.com>
2025-04-05 15:03:24 +0200

    batch 16 to compare speed

--- FasterRCNN-PyTorch-main/tools/train.py
+++ FasterRCNN-PyTorch-main/tools/train.py
@@ -114,7 +114,7 @@ def train(args):
                      ann_dir=dataset_config['ann_train_path'])
     
     train_dataset = DataLoader(voc,
-                               batch_size=18,
+                               batch_size=64,
                                shuffle=True,
                                num_workers=2,
                                collate_fn=collate_fn
